5f957d9f2635d2bd14bf0053bcfe6454528cde9f
Craig Sorensen <draker@draker.net>
2022-07-20 22:24:19 -0700

    Revert "monster commit"
    This reverts commit d22daafae0e38c696f6c3541ec74e1a4d82e8e8a.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import requests
 import os
 import logging
-import json
 
 from datetime import datetime
 from pages.show_pages import ShowPage
@@ -9,67 +8,76 @@ from send_push import push
 
 SHOW_URL = "https://www.vulcanatx.com/"
 CRED_DIR = os.path.expanduser("~")
-api_cred_file = "{0}/.killtony_push_api.txt".format(CRED_DIR)
+api_cred_file = "{0}/.howard_push_api.txt".format(CRED_DIR)
 SCRIPT_EXC_DIR = os.path.dirname(os.path.realpath(__file__))
-database = "{0}/database.json".format(SCRIPT_EXC_DIR)
+lock_file = "{0}/push.lock".format(SCRIPT_EXC_DIR)
 log_dir = f'{SCRIPT_EXC_DIR}/app.log'
 
 logging.basicConfig(filename=log_dir, format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
 
+
+
+date = datetime.now()
+formatted_date = date.strftime("%b %d, %Y")
+f_date = datetime.strptime(formatted_date, "%b %d, %Y")
+
 # Get page content
 page_content = requests.get(SHOW_URL).content
-#fakepage = "{0}\\fakepage\\fakepage.html".format(SCRIPT_EXC_DIR)
-#with open(fakepage, 'r') as f:
-#    print("reading fakepage from disk")
-#    page_content = f.read()
-
 
 # Get the show elements from the page
 page = ShowPage(page_content)
 
 shows = page.shows
+print(shows)
 
-# open show db file
-with open(database, 'r') as f:
-    print("reading show database from disk")
-    db = f.read()
-
-show_db = json.loads(db)
+# Check if lock exists and confirm validity
+# if os.path.isfile(lock_file):
+#     with open(lock_file, 'r') as f:
+# 	    lock_date = f.readline().strip()
+#     if lock_date == date.strftime("%b %d, %Y"):
+#         print("Found lockfile. Has notification already been sent?")
+#         logging.info("Found lockfile. Has notification already been sent today?")
+#         logging.info(f"Lockfile Location: {lock_file}")
+#         logging.info(f"Lockdate: {lock_date}")
+#         quit("quitting")
+#     else:
+#         print("Old lockfile found, removing!")
+#         logging.info("Old lockfile found, removing!")
+#         os.remove(lock_file)
 
 # Get push API credentials from disk
-if os.path.isfile(api_cred_file):
-    with open(api_cred_file, 'r') as f:
-        creds = f.readlines()
-    TOKEN = creds[0].strip().split(':')[1]
-    USER = creds[1].strip().split(':')[1]
-else:
-    print(f"No API Credentials found in: {api_cred_file} - Check README file for setup instuctions")
-    logging.info(f"No API Credentials found in: {api_cred_file} - Check README file for setup instuctions")
+# if os.path.isfile(api_cred_file):
+    # with open(api_cred_file, 'r') as f:
+        # creds = f.readlines()
+    # TOKEN = creds[0].strip().split(':')[1]
+    # USER = creds[1].strip().split(':')[1]
+# else:
+    # print(f"No API Credentials found in: {api_cred_file} - Check README file for setup instuctions")
+    # logging.info(f"No API Credentials found in: {api_cred_file} - Check README file for setup instuctions")
 
-
-show_list = []
 new_show = False
-# Add filtered shows listed on webpage to list
+# Check shows, alert if new 
 for show in shows:
-    if (show.date[1].lower()) == 'oct' and (show.topic).lower() == 'kill tony':
-        print(f'{show.date} - {show.topic}')
-        show_list.append(show.date)
-
-for show in show_list:
-    if show not in show_db['shows']:
-        show_db['shows'].append(show)
+    compare_show_date = datetime.strptime(show.date, "%b %d, %Y")
+    if compare_show_date == f_date:
+        print(f"New Show Found! for {compare_show_date}")
+        logging.info(f'New Show Found! for {compare_show_date}')
+        print("Sending push notification...")
+        logging.info("Sending push notification...")
+        print(f'Topics: {show.topics}')
+        logging.info(f'Topics: {show.topics}')
         new_show = True
-
-if new_show:
-    print('sending notification')
-    push.send(TOKEN, USER, f"New KILL TONY show added. DATE: {show_db['shows'][-1]}")
-    with open(database, 'w+') as f:
-        print("Writing DB")
-        f.write(json.dumps(show_db))
+        push.send(TOKEN, USER, show.topics)
+        
+        with open(lock_file, 'w+') as f:
+            logging.debug("writing lock file")
+            f.write(formatted_date)
 
 if not new_show:
-    print("No new show found")
-    logging.info("No new show found}")
+    print(f"No new show found for {f_date}")
+    logging.info(f"No new show found for {str(f_date)}")
+   
+
 
 
 
